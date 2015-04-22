a3a1834c3fdc863a6e692ce7ab1b3aef7ec26f6f
hsiaoyi <hsiaoyi.chang@gmail.com>
2015-04-22 22:16:32 +0800

    update

--- Resources/Demo/mdstart.lua
+++ Resources/Demo/mdstart.lua
@@ -5,10 +5,10 @@ if ctr == 1 then
 	--SetGameBG("Demo/helloworld.png");
 	
 	SetTitleFont("fonts/NotoSansCJKtc-Light.otf", 24);
-	SetTitleText("第一章 中文\n第二行 標題測試");
+	SetTitleText("第一章 中文\n第二行 標題測試\n第三行 一樣也OK");
 	SetTileTextWordByWordEffect(0.05, 1.0, true);--period, delay, repeat
 	
-	SetTitleAlignWin(2,2); --hoti 0:left, 1:center, 2:right
+	SetTitleTextAlignWin(2,2); --hoti 0:left, 1:center, 2:right
 								--vert 0:top, 1:center, 2:buttom
 	--SetTitlePos()//top, center
 	--SetTitleColor();
