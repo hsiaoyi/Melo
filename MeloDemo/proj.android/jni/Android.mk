c5ff623cdc55a7742a80f5779766dadf70f8482b
hsiaoyi <hsiaoyis@gmail.com>
2015-10-30 12:34:33 +0800

    update

--- MeloDemo/proj.android/jni/Android.mk
+++ MeloDemo/proj.android/jni/Android.mk
@@ -17,7 +17,6 @@ LOCAL_SRC_FILES := MeloDemo/melomain.cpp \
                    ../../MeloDemo/MDAppDelegate.cpp \
                    ../../MeloDemo/MDGameScene.cpp \
                    ../../MeloDemo/MDLuaWrapper.cpp \
-                   #../../MeloDemo/HelloWorldScene.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../MeloDemo \
 					$(LOCAL_PATH)/../../../Melo \
@@ -25,8 +24,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../MeloDemo \
 					$(LOCAL_PATH)/../../../cocos2d/external/lua/luajit/include \
 					$(LOCAL_PATH)/../../../cocos2d/external/freetype2/include/android/freetype2 \
 
-#LOCAL_STATIC_LIBRARIES := cocos2dx_static
-LOCAL_STATIC_LIBRARIES := melo_static
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_STATIC_LIBRARIES += melo_static
 
 LOCAL_CFLAGS   :=  -DMELO_SUPPORT
 LOCAL_CFLAGS   +=  -DUSE_COCOS2DX
@@ -34,5 +33,5 @@ LOCAL_CFLAGS   +=  -DUSE_COCOS2DX
 include $(BUILD_SHARED_LIBRARY)
 #==============================================================
 #$(call import-module,.)
-#$(call import-module,cocos)
+$(call import-module,cocos)
 $(call import-module,Melo)
