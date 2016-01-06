4cf5e4f5e78cf6b7e453c6e8e04e48f289606684
Feynman <feynmansu@hotmail.com>
2016-01-06 20:06:13 +0800

    fix android build

--- Melo/MLUtility.cpp
+++ Melo/MLUtility.cpp
@@ -6,112 +6,125 @@
 //	Copyright (c) 2014-2015. All rights reserved.
 //	https://github.com/hsiaoyi/Melo
 //--------------------------------------------------------------------------------
-#if defined(ML_ANDROID)
-
 #include "MLUtility.h"
 #include "MLUtilityJni.h"
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getUDIDForVendor(const std::string path, const std::string secretKey)
 {
     return getUDIDForVendorJni(path, secretKey);
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getPlatform()
 {
     return "android";
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getDevice()
 {
     return getDeviceJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getDeviceName()
 {
     return getDeviceNameJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getOSVersion()
 {
     return getOSVersionJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getAppVersionName()
 {
     return getAppVersionNameJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getAppVersionCode()
 {
     return getAppVersionCodeJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getCountryCode()
 {
     return getCountryCodeJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getCurrencyCode()
 {
     return getCurrencyCodeJni();
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getAdvertisementID()
 {
     return getAdvertisementIDJni();
 }
 
-///////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getApplicationRoot()
 {
     return "";
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getResource()
 {
     return "";
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getDocuments()
 {
     return "/Documents/";
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getTemp()
 {
     return "/Temp/";
 }
 
-///////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------
 std::string MLUtility::getIdentifier(const std::string& path, const std::string& secretKey)
 {
     return getIdentifierJni(path, secretKey);
 }
 
+//--------------------------------------------------------------------------------
 bool MLUtility::setIdentifier(const std::string& path, const std::string& identifier, const std::string& secretKey)
 {
     return setIdentifierJni(path, identifier, secretKey);
 }
 
+//--------------------------------------------------------------------------------
 bool MLUtility::deleteIdentifier(const std::string& path)
 {
     return deleteIdentifierJni(path);
 }
 
+//--------------------------------------------------------------------------------
 bool MLUtility::hasIdentifier(const std::string& path)
 {
     return hasIdentifierJni(path);
 }
 
-///////////////////////////////////////////////////////////////////////////////
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getCertKey(const std::string &rndCode)
 {
     return getCertKeyJni(rndCode);
 }
 
+//--------------------------------------------------------------------------------
 const std::string MLUtility::getSha1(const std::string src, unsigned int digestLength)
 {
-    return getSha1Jni(srcㄝ, digestLength);
+    return getSha1Jni(src, digestLength);
 }
-
-#endif
