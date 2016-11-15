042973056f35ba7c50d5e21b2424a0d0da5c2cc5
Feynman <feynmansu@hotmail.com>
2016-11-15 09:29:13 +0800

    fix local notification

---
+++ proj.ios/AppDelegate.h
@@ -0,0 +1,18 @@
+//--------------------------------------------------------------------------------
+//	File		: AppDelegate.h
+//	Description	: For initial local push notification use
+//	Author		: Chang, Hsiao-Yi
+//
+//	Copyright (c) 2014-2015. All rights reserved.
+//	https://github.com/hsiaoyi/Melo
+//--------------------------------------------------------------------------------
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ proj.ios/ViewController.h
@@ -0,0 +1,23 @@
+//--------------------------------------------------------------------------------
+//	File		: ViewController.h
+//	Description	: For initial local push notification use
+//	Author		: Chang, Hsiao-Yi
+//
+//	Copyright (c) 2014-2015. All rights reserved.
+//	https://github.com/hsiaoyi/Melo
+//--------------------------------------------------------------------------------
+
+#import <UIKit/UIKit.h>
+
+#define XCODE_VERSION_GREATER_THAN_OR_EQUAL_TO_8    __has_include(<UserNotifications/UserNotifications.h>)
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+@interface ViewController : UIViewController
+
+
+@end
+
